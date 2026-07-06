90c06ed69d74d59b292a66bc1136e0dfb5710b4e
Fernando Claverino <fernando.claverino@gmail.com>
2026-07-06 17:07:27 -0300

    feat(light-service): add hello response schema

---
+++ light-service/src/model/hello.model.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const helloSchema = {
+  response: {
+    200: z.object({
+      message: z.string(),
+      instance: z.number(),
+    }),
+  },
+};
+
+export type HelloResponse = z.infer<typeof helloSchema.response["200"]>;
