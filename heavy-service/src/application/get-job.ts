e8f00ecaa8f8b3c4caa90f9c91de9a8675c22a28
Fernando Claverino <fernando.claverino@gmail.com>
2026-07-20 11:38:27 -0300

    fix(heavy-service): return null instead of undefined from GetJob

--- heavy-service/src/application/get-job.ts
+++ heavy-service/src/application/get-job.ts
@@ -4,7 +4,7 @@ import { JobRepository } from "./ports/job-repository";
 export class GetJob {
   constructor(private jobRepository: JobRepository) {}
 
-  async execute(jobId: JobId): Promise<Job | undefined> {
+  async execute(jobId: JobId): Promise<Job | null> {
     return this.jobRepository.getById(jobId);
   }
 }
